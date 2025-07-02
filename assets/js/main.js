99498569f55630899b101ae80cf3b85ac1832e3a
1fu <75655210+1fu@users.noreply.github.com>
2025-07-02 16:49:45 +0800

    Remove fuckass mentis menu and extra script tag

--- assets/js/main.js
+++ assets/js/main.js
@@ -141,14 +141,14 @@ $(function () {
 
 
 
-  /* menu active */
+  /* menu active 
 
   $(function () {
     for (var e = window.location, o = $(".metismenu li a").filter(function () {
       return this.href == e
     }).addClass("").parent().addClass("mm-active"); o.is("li");) o = o.parent("").addClass("mm-show").parent("").addClass("mm-active")
   });
-
+*/
 
 
 });
