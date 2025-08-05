937f27aba1820c943304bdbb5befa93e6c1762bc
1fu <75655210+1fu@users.noreply.github.com>
2025-08-05 20:33:02 +0800

    Performance: Add preconnect hints for faster resource loading

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,13 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         
+        {/* Preconnect hints for faster resource loading */}
+        <link rel="preconnect" href="https://cdn.jsdelivr.net" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link rel="preconnect" href="https://www.googletagmanager.com" />
+        <link rel="preconnect" href="https://code.jquery.com" />
+        
         {/* favicon */}
         <link rel="icon" href="/assets/images/favicon.ico" type="image/x-icon" />
         
