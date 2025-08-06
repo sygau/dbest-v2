bce1133e3f1195589803db2a3963b4a01c02c6b2
1fu <75655210+1fu@users.noreply.github.com>
2025-08-06 13:14:37 +0800

    2.1.4 min js

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -70,8 +70,8 @@ export default function Document() {
         {/* Critical Scripts - Load in dependency order */}
         <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="/assets/js/main.js" defer></script>
-        <script src="/assets/js/appendLinks.js" defer></script>
+        <script src="/assets/js/main.min.js" defer></script>
+        <script src="/assets/js/appendLinks.min.js" defer></script>
       </body>
     </Html>
   )
