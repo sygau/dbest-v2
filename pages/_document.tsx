bbaba3b7d3501bd31916126fe42d5a1d9cc7f9a2
1fu <75655210+1fu@users.noreply.github.com>
2025-08-23 20:57:39 +0800

    no preloading font apparaenty

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,7 +19,7 @@ export default function Document() {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
         
         {/* Fonts */}
-        <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600&display=swap" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600&display=swap" rel="stylesheet" />
         
         {/* Combined CSS - All themes and styles in one file */}
         <link rel="preload" href="/assets/css/combined.min.css" as="style" />
