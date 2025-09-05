e0f2b24bacbbcea17490d7d57569a86b06d90dbf
1fu <75655210+1fu@users.noreply.github.com>
2025-09-06 01:12:53 +0800

    mauhong

--- utils/changelogData.ts
+++ utils/changelogData.ts
@@ -6,6 +6,8 @@ export interface ChangelogEntry {
 export const changelogData: ChangelogEntry = {
   date: "6/9/2025",
   changes: [
-    "2025 Chem, Math, and Chinese pp added"
+    "2025 Chem, Math, and Chinese pp added",
+    "CSD page added, 4 papers",
+    "New stickers in chatroom"
   ]
 }; 
