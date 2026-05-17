4b5df846bcc51f93766e362af412c3242f3dec58
1fu <75655210+1fu@users.noreply.github.com>
2026-05-17 19:34:03 +0900

    Preload back on

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -4,6 +4,6 @@ export const notoSansHK = Noto_Sans_HK({
   subsets: ['latin'],
   weight: ['400', '700'],
   display: 'optional',
-  preload: false,
+  preload: true,
   variable: '--font-noto-sans-hk',
 })
