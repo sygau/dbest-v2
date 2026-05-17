c09e08043287b81a68787c3c2a107015226fc218
1fu <75655210+1fu@users.noreply.github.com>
2026-05-17 15:22:57 +0900

    font optional ig

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -3,7 +3,7 @@ import { Noto_Sans_HK } from 'next/font/google'
 export const notoSansHK = Noto_Sans_HK({
   subsets: ['latin'],
   weight: ['400', '700'],
-  display: 'block',
+  display: 'optional',
   preload: true,
   variable: '--font-noto-sans-hk',
 })
